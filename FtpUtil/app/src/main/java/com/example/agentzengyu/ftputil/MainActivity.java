754f6bb00cc71e9bdc1b2e5e2d545ea5ca434a99
13608089849 <1213814232@qq.com>
2017-05-29 17:23:32 +0800

    update

--- FtpUtil/app/src/main/java/com/example/agentzengyu/ftputil/MainActivity.java
+++ FtpUtil/app/src/main/java/com/example/agentzengyu/ftputil/MainActivity.java
@@ -13,10 +13,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        File file = new File("D:\\LIBRARY_ESCAPE");
-        System.out.println("getAbsolutePath"+file.getAbsolutePath());
-        FtpUtil ftpUtil = new FtpUtil();
-        ftpUtil.SetParams(21, "35.48.82.15", "ropeokRead", "1234", "\\LIBRARY_ESCAPE", "D:\\LIBRARY_ESCAPE");
-        ftpUtil.Download();
     }
 }
